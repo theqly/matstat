7d8d605747909f85585ef373f357a64debd1ef2e
Artem <theqlgames@gmail.com>
2024-06-18 17:43:07 +0700

    sync

--- calculation/src/fourth/task2.py
+++ calculation/src/fourth/task2.py
@@ -7,7 +7,7 @@ def chi2_ro_standard_uniform(k, n, array):
         vi = 0
         for j in array:
             #print("[" + str((i - 1) * (1 / k)) + "; " + str(i * (1 / k)) + "]")
-            if (i - 1) * (1 / k) <= j <= i * (1 / k):
+            if (i - 1) * (1 / k) <= j < i * (1 / k):
                 vi += 1
         #print(vi)
         sum += (vi - n * (1 / k)) ** 2 / (n * (1 / k))
@@ -18,18 +18,20 @@ file = open("../../data/uniform_distribution.txt")
 data = []
 n = 30
 e = 0.18
-c = stats.chi2.ppf(1 - e, n - 1)
+c = stats.chi2.ppf(1 - e, 5 - 1)
 print(c)
 
 for line in file:
     numbers = [float(x) for x in line.strip().split()]
     data.extend(numbers)
 
+data.sort()
+print(data)
 assert (len(data) == n)
 
-ro = chi2_ro_standard_uniform(n, n, data)
+ro = chi2_ro_standard_uniform(5, n, data)
 
-p_value = 1 - stats.chi2.cdf(ro, n)
+p_value = 1 - stats.chi2.cdf(ro, 5 - 1)
 
 print("d_hi: " + str(ro))
 print("p_value: " + str(p_value))
